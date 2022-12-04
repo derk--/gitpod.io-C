055430fceb199593e9692e98ea54d736696e47b3
Derek Doran <derek.doran@wright.edu>
2022-12-04 02:41:18 +0000

    day 1 challenge

--- advent-of-code-2022/day1/day1-2.c
+++ advent-of-code-2022/day1/day1-2.c
@@ -16,26 +16,26 @@ int min_index(int* arr){
     return idx;
 }
 
-void update_cals(int* max_cals, int cal_count){
+void update_cals(int* max_cals, int* cal_count){
     int idx = min_index(max_cals);
-    max_cals[idx] = cal_count >= max_cals[idx] ? cal_count : max_cals[idx];
+    max_cals[idx] = *cal_count >= max_cals[idx] ? *cal_count : max_cals[idx];
+    *cal_count = 0;
 }
 
 int main(){
     FILE* input_file = fopen("day1-input.txt", "r");
     char line_buffer[7];
-    int max_cals[] = {-1, -1, -1};
+    int max_cals[3] = {-1};
     int cal_count = 0;
     while(fgets(line_buffer, MAX_LINE_LENGTH, input_file)){
         if(line_buffer[0] == '\n'){
-            update_cals(max_cals, cal_count);
-            cal_count = 0;
+            update_cals(max_cals, &cal_count);
         }
         else{
             cal_count += atoi(line_buffer);
         }
     }
-    update_cals(max_cals, cal_count);
+    update_cals(max_cals, &cal_count);
 
     printf("Top3 cals: %d, %d, %d; Max calroies: %d\n", max_cals[0], max_cals[1], max_cals[2], max_cals[0] + max_cals[1] + max_cals[2]);
     return 0;
