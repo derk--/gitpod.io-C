dea901485fa2fe54c5f4c95dbb73e256eae74820
Derek Doran <derek.doran@wright.edu>
2022-12-29 13:17:53 +0000

    day5-1

--- advent-of-code-2022/day5/day5-1.c
+++ advent-of-code-2022/day5/day5-1.c
@@ -3,7 +3,8 @@
 #include <stdbool.h>
 #include <string.h>
 
-#define STACK_COUNT 3
+#define STACK_COUNT 9
+#define BUFFER_SIZE 1024
 
 void push(char* stack, char item){
     char str_item[2] = {item, '\0'};
@@ -11,14 +12,21 @@ void push(char* stack, char item){
 }
 
 char pop(char* stack){
-    char ret = stack[strlen(stack) - 1];
-    stack[strlen(stack) - 1] = '\0';
+    int len = strlen(stack);
+    char ret = stack[len-1];
+    stack[len-1] = '\0';
     return ret;
+}
+
+void print_stacks(char* stacks[STACK_COUNT]){
+    for(int i=0; i < STACK_COUNT; i++){
+        printf("%s\n", stacks[i]);
+    }
 }
     
 int calculate_max_stack(FILE* input_file){
     fseek(input_file, 0, SEEK_SET);
-    char input_line[100];
+    char input_line[BUFFER_SIZE];
     int max_size = 0;
     while(fgets(input_line, 100, input_file)){
         for (int i=0; input_line[i] != '\0'; i++){
@@ -28,44 +36,63 @@ int calculate_max_stack(FILE* input_file){
     return max_size;
 }
 
+/* We need this routine because when we read the stack setup from the file, 
+   we read the element at the top of the stack first, going down to the bottom.
+   So we can't push, when we read a new value during setup, we need to add it 
+   _to the bottom_ of the stack. */
+void stack_add_during_init(char* stack, char add){
+    char to_push[2] = {add, '\0'};
+    memmove(stack + 1, stack, strlen(stack) + 1);
+    memcpy(stack, to_push, 1);
+}
+
 void init_stacks(char* stacks[STACK_COUNT], FILE* input_file){
-    char input_line[100];
+    char input_line[BUFFER_SIZE];
     for(int i=0; i < STACK_COUNT; i++){
         stacks[i] = (char *)malloc(sizeof(char) * calculate_max_stack(input_file));
     }
     fseek(input_file, 0, SEEK_SET);
-    while(fgets(input_line,100,input_file)){
-        if(input_line[0] == '['){
-            push(stacks[0], input_line[1]);
-        }
-        if(input_line[4] == '['){
-            push(stacks[1], input_line[5]);
-        }
-        if(input_line[8] == '['){
-            push(stacks[2], input_line[9]);            
+    while(fgets(input_line,BUFFER_SIZE,input_file)){
+        for(int i=0; i < strlen(input_line); i++){
+            if(input_line[i] == '[')
+                stack_add_during_init(stacks[i/4], input_line[i+1]);         
         }
     }
 }
 
-void print_stacks(char* stacks[STACK_COUNT]){
-    for(int i=0; i < STACK_COUNT; i++){
-        printf("%s\n", stacks[i]);
+void move_items(char* stacks[STACK_COUNT], FILE* input_file){
+    char input_line[BUFFER_SIZE], val;
+    int move_count, source, target;
+
+    fseek(input_file, 0, SEEK_SET);
+    while(fgets(input_line,BUFFER_SIZE,input_file)){
+        if(input_line[0] == 'm'){
+            sscanf(input_line, "move %d from %d to %d", &move_count, &source, &target);
+            for(int i=0; i < move_count; i++){
+                val = pop(stacks[source-1]);
+                push(stacks[target-1], val);
+                printf("stacks after move from %d to %d:\n", source, target);
+                print_stacks(stacks);
+            }
+        }
     }
 }
+
 
 int main(){
 
-    FILE* input_file = fopen("test.txt","r");
+    FILE* input_file = fopen("day5-input.txt","r");
 
     char* stacks[STACK_COUNT];
-    rewind(input_file);
-
     init_stacks(stacks, input_file);
 
     printf("Initial stacks:\n");
     print_stacks(stacks);
 
-    fseek(input_file, 0, SEEK_SET);
+    move_items(stacks, input_file);
+
+    printf("Final stacks: \n");
+    print_stacks(stacks);
 
     return 0;
 }
