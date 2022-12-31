90ef9a65ebc3b936ac19a3eea302537405447018
Derek Doran <derek.doran@wright.edu>
2022-12-31 01:09:13 +0000

    day6-2

--- advent-of-code-2022/day6/day6-2.c
+++ advent-of-code-2022/day6/day6-2.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 #define WINDOW_SIZE 4
+#define SOM_MARKER_SIZE 14
 
 int main(){
     char* filename = "day6-input.txt";
@@ -15,14 +16,25 @@ int main(){
     char* ds_buffer = (char*)malloc(sizeof(char)*size + 1);
     fgets(ds_buffer, size+1, input_file);
     printf("buffer: %s\n", ds_buffer);
-    char w[4] = {ds_buffer[0], ds_buffer[1], ds_buffer[2], ds_buffer[3]};
-    int k = 4;
+    char w[SOM_MARKER_SIZE]; 
+    for(int i=0; i<SOM_MARKER_SIZE; i++){
+        w[i] = ds_buffer[i];
+    }
+    int k = SOM_MARKER_SIZE;
     int not_found=1;
     for(k; k < size; k++){
         printf("k=%d, current window: %s\n", k, w);
-        not_found = (w[0] == w[1] || w[0] == w[2] || w[0] == w[3] ||
-                     w[1] == w[2] || w[1] == w[3] || 
-                     w[2] == w[3]);
+        for(int i=0; i<SOM_MARKER_SIZE; i++){
+            for(int j=0; j < SOM_MARKER_SIZE; j++){
+                not_found = w[i] == w[j]; //doesnt quite work.
+                if(!not_found){
+                    break;
+                }
+            }
+            if(!not_found){
+                break;
+            }
+        }
         if(not_found){
             w[0] = w[1]; 
             w[1] = w[2];
