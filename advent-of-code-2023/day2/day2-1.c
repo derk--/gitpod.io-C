41efadc271ec81831b30404f0d30ffcf459b4a79
Derek Doran <derek.doran@wright.edu>
2023-12-15 20:14:51 +0000

    updates

--- advent-of-code-2023/day2/day2-1.c
+++ advent-of-code-2023/day2/day2-1.c
@@ -4,47 +4,57 @@
 
 #define MAX_LINE_SIZE 1024
 
+void replace_semi(char* string){
+    char* semi_loc = strchr(string, ';');
+    while(semi_loc){
+        *semi_loc = ',';
+        semi_loc = strchr(string, ';');
+    }
+}
+
 int game_plausible(char* string, int max_red, int max_green, int max_blue){
     //game, red, green, blue
     //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+
+    replace_semi(string);
+    printf("%s", string);
 
     //get the game number
     char* cur_str_ptr = strchr(string, ':');
     char game_label[cur_str_ptr - string]; 
     strncpy(game_label, string, cur_str_ptr - string);
     int game_num = strtol(strchr(game_label, ' ')+1, NULL, 10);
-    printf("%d\n", game_num);
 
     //move the pointer index up to the start of the first pull
     cur_str_ptr = cur_str_ptr + 2;
     
-    char* pull_str_end = strchr(cur_str_ptr, ';');
-    do{
-        while(cur_str_ptr < pull_str_end){
-                printf("%X\n", cur_str_ptr);
-                printf("%X\n", pull_str_end);
+    while(cur_str_ptr){
 
-                char* val_ptr = strchr(cur_str_ptr, ' ');
-                printf("%X\n", val_ptr);
+        char* eos = strchr(cur_str_ptr, '\0');
+        printf("%s", *eos);
+        char* val_ptr = strchr(cur_str_ptr, ' '); 
+        printf("%s", cur_str_ptr);
 
-                char val_buffer[val_ptr - cur_str_ptr];
-                strncpy(val_buffer, cur_str_ptr, val_ptr - cur_str_ptr);
-                int val = strtol(val_buffer, NULL, 10);
-                printf("%d\n", val);
-                cur_str_ptr = val_ptr;
+        char val_buffer[val_ptr - cur_str_ptr];
+        strncpy(val_buffer, cur_str_ptr, val_ptr - cur_str_ptr);
 
-                char* color_ptr = strchr(cur_str_ptr, ',');
-                char color_buffer[color_ptr - cur_str_ptr];
-                strncpy(color_buffer, cur_str_ptr+1, color_ptr - cur_str_ptr-1); 
-                if((color_buffer[0] == 'r' && val > max_red)   ||
-                (color_buffer[0] == 'g' && val > max_green) ||
-                (color_buffer[0] == 'b' && val > max_blue)  ){
-                    return 0;
-                }
-                cur_str_ptr = color_ptr + 2;
-            }
-            pull_str_end = strchr(cur_str_ptr, ';');
-    } while(pull_str_end);
+        int val = strtol(val_buffer, NULL, 10);
+        printf("%d\n", val);
+        cur_str_ptr = val_ptr;
+        printf("Now cur_str_ptr: %s", cur_str_ptr);
+
+        char* color_ptr = strchr(cur_str_ptr, ',');
+        char color_buffer[color_ptr - cur_str_ptr];
+        strncpy(color_buffer, cur_str_ptr+1, color_ptr - cur_str_ptr); 
+
+        printf("%s\n", color_buffer);
+        if((color_buffer[0] == 'r' && val > max_red)   ||
+        (color_buffer[0] == 'g' && val > max_green) ||
+        (color_buffer[0] == 'b' && val > max_blue)  ){
+            return 0;
+        }
+        cur_str_ptr = color_ptr + 2;
+    }
     return game_num;
 }
 
