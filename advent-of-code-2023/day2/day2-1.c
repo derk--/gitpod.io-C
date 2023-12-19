81dc0ffe0c6efa83c42b182dff05e8efdeb648fe
Derek Doran <derek.doran@wright.edu>
2023-12-19 16:27:36 +0000

    day2-1 solved

--- advent-of-code-2023/day2/day2-1.c
+++ advent-of-code-2023/day2/day2-1.c
@@ -12,19 +12,19 @@ void replace_semi(char* string){
     }
 }
 
-void clear_buffer(char* buf){
-    for(int i = 0; i < MAX_LINE_SIZE; i++){
+void clear_buffer(char* buf, int size){
+    for(int i = 0; i < size; i++){
         buf[i] = 0;
     }
 }
 
 int game_plausible(char* string, int max_red, int max_green, int max_blue){
     //game, red, green, blue
-    //Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 
     replace_semi(string);
     printf("%s", string);
     char color_buffer[MAX_LINE_SIZE];
+    char val_buffer[MAX_LINE_SIZE];
 
     //get the game number
     char* cur_str_ptr = strchr(string, ':');
@@ -35,18 +35,16 @@ int game_plausible(char* string, int max_red, int max_green, int max_blue){
     //move the pointer index up to the start of the first pull
     cur_str_ptr = cur_str_ptr + 2;
     
-    while(cur_str_ptr){
-
-        printf("Now cur_str_ptr: %s", cur_str_ptr);
-
+    while(*cur_str_ptr != '\0'){
 
         char* val_ptr = strchr(cur_str_ptr, ' '); 
-        char val_buffer[val_ptr - cur_str_ptr];
+        clear_buffer(val_buffer, MAX_LINE_SIZE);
         strncpy(val_buffer, cur_str_ptr, val_ptr - cur_str_ptr);
         int val = strtol(val_buffer, NULL, 10);
-        printf("%d\n", val);
+        //got the pull color val. advance the pointer to the name of the color.
+        cur_str_ptr = cur_str_ptr + (val_ptr - cur_str_ptr) + 1;
 
-        clear_buffer(color_buffer);
+        clear_buffer(color_buffer, MAX_LINE_SIZE);
         char* color_ptr = 0;
         int i =0; 
         while(*cur_str_ptr != '\0'){
@@ -59,7 +57,6 @@ int game_plausible(char* string, int max_red, int max_green, int max_blue){
             i++;
         } 
         if(color_ptr){
-            printf("%s\n", color_buffer);
             if((color_buffer[0] == 'r' && val > max_red)   ||
             (color_buffer[0] == 'g' && val > max_green) ||
             (color_buffer[0] == 'b' && val > max_blue)  ){
@@ -72,9 +69,15 @@ int game_plausible(char* string, int max_red, int max_green, int max_blue){
 }
 
 int main(){
-    FILE* input_file = fopen("day2-input.txt", "r");
+    FILE* input_file = fopen("/workspace/gitpod.io-C/advent-of-code-2023/day2/day2-input.txt", "r");
     char line[MAX_LINE_SIZE];
+    int sum = 0;
+    int game_ok = -1;
     while(fgets(line, MAX_LINE_SIZE, input_file)){
-        printf("--> %d\n", game_plausible(line, 12, 13, 14));
+        game_ok = game_plausible(line, 12, 13, 14); 
+        printf("--> %d\n", game_ok);
+        sum += game_ok;
+        printf("Solution: %d\n", sum);
+
     }
 }
